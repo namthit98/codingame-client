bbdb882de4496275b4fc4c1f85d43063be394174
To Hai Nam <namthit98@MacBook-Pro.local>
2020-05-12 03:20:46 +0700

    add env

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://quiet-waters-32044.herokuapp.com" || 'http://localhost:7000'
+export const BASE_URL = "https://quiet-waters-32044.herokuapp.com/" || 'http://localhost:7000'
